31989b57563912246e22c795a59af434d4906952
David R <iniciativad@gmail.com>
2015-03-24 14:30:39 -0300

    Configuração AutoMapper e Add pasta ViewModel

---
+++ GrupoPlennus/app/GrupoPlennus.HabitacaoMvc/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GrupoPlennus.Domain.Entities;
+using GrupoPlennus.HabitacaoMvc.ViewModels;
+
+namespace GrupoPlennus.HabitacaoMvc.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappingProfile"; }
+        }
+
+        protected override void Configure()
+        {
+            
+            Mapper.CreateMap<ConjugeViewModel, Conjuge>();
+            Mapper.CreateMap<DependenteViewModel, Dependente>();
+            Mapper.CreateMap<EntidadeViewModel, Entidade>();
+            Mapper.CreateMap<EntidadeMasterViewModel, EntidadeMaster>();            
+            Mapper.CreateMap<PessoaViewModel, Pessoa>();
+           
+           
+        }
+    }
+}
---
+++ GrupoPlennus/app/GrupoPlennus.HabitacaoMvc/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GrupoPlennus.Domain.Entities;
+using GrupoPlennus.HabitacaoMvc.ViewModels;
+
+namespace GrupoPlennus.HabitacaoMvc.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappingProfile"; }
+        }
+
+        protected override void Configure()
+        {
+          
+            Mapper.CreateMap<Conjuge, ConjugeViewModel>();
+            Mapper.CreateMap<Dependente, DependenteViewModel>();
+            Mapper.CreateMap<Entidade, EntidadeViewModel>();
+            Mapper.CreateMap<EntidadeMaster, EntidadeMasterViewModel>();
+           
+            Mapper.CreateMap<Pessoa, PessoaViewModel>();
+          
+        }
+    }
+}
