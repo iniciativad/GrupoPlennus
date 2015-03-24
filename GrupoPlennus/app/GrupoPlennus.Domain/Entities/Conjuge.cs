e3ffc7a4d9f59d8598dfec1f66e2c11ed10534f3
David R <iniciativad@gmail.com>
2015-03-24 10:49:49 -0300

    Add Projeto Domain - Add Class Conjuge, Dependente, Entidade, EntidadeMaster, Pessoa

---
+++ GrupoPlennus/app/GrupoPlennus.Domain/Entities/Conjuge.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace GrupoPlennus.Domain.Entities
+{
+   public class Conjuge
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Nis { get; set; }
+        public int SexoId { get; set; }
+        public int NacionalidadeId { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string RgNum { get; set; }
+        public string RgOrgaoEmissor { get; set; }
+        public string RgSiglaOrgaoEmissor { get; set; }
+        public string RgDataExpedicao { get; set; }
+        public string Pis { get; set; }
+        public int EstadoCivilId { get; set; }
+        public string Profissao { get; set; }
+        public int AtividadeId { get; set; }
+        public int TempoTrabalhoAtividadeAnos { get; set; }
+        public int TempoTrabalhoAtividadeMeses { get; set; }
+        public string LocalOndeExerceAtividade { get; set; }
+        public string BancoAgencia { get; set; }
+        public string BancoOperacao { get; set; }
+        public string BancoConta { get; set; }
+        public bool ContaConjuta { get; set; }
+        public int NaturalidadeId { get; set; }
+        public bool MenorEmancipado { get; set; }
+        public bool MenorAssistido { get; set; }
+        public int GrauInstrucaoId { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public string Telefone { get; set; }
+        public int ResidenciaAtualId { get; set; }
+        public int RendaComprovadaId { get; set; }
+        public int RendaNaoComprovadaId { get; set; }
+        public int UltimoEmpregoAnos { get; set; }
+        public int UltimoEmpregoMes { get; set; }
+        public int UltimoEmpregoAnoDesligamento { get; set; }
+        public string CargoFuncao { get; set; }
+        public int DocumentosComplementares { get; set; }
+        public int InformacoesPatrimoniaisId { get; set; }
+        public bool PossuiImoveis { get; set; }
+        public bool PossuiVeiculos { get; set; }
+        public int CartaoCreditoId { get; set; }
+        public int ContaOutrosBancosId { get; set; }
+        public int AplicacoesForaCaixaId { get; set; }             
+        public int EntidadeId { get; set; }
+        public int PessoaId { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ GrupoPlennus/app/GrupoPlennus.Domain/Entities/Entidade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GrupoPlennus.Domain.Entities
+{
+    public class Entidade
+    {
+       
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        
+        public string Cnpj { get; set; }
+
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public string Cep { get; set; }
+
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+       
+        public string Responsavel { get; set; }
+        public string ResponsavelCpf { get; set; }
+        public string ResponsavelCargo { get; set; }
+        public string TipoEntidade { get; set; }
+        public DateTime DataFundacao { get; set; }
+        public string OutrasExperienciasHabitacao { get; set; }
+        public string FiliacaoMovimentoNacional { get; set; }
+        public string Banco { get; set; }
+        public string BancoAgencia { get; set; }
+        public string BancoConta { get; set; }
+        public string BancoOperacao { get; set; }
+        public int EntidadeMasterId { get; set; }
+        public virtual EntidadeMaster EntiadeMaster { get; set; }
+        public virtual IEnumerable<Pessoa> Pessoas { get; set; }
+    }
+}
